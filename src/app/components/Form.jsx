a2326c4e54fd00fb2394e0585f9028bd10016ec1
saif <saif@gmail.com>
2025-03-21 16:24:13 +0530

    add

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,20 +1,27 @@
 "use client"
-export const Form=()=>{
-    const handleSubmit=(e)=>{
+import {motion} from 'framer-motion'
+export const Form = () => {
+    const handleSubmit = (e) => {
         e.preventDefault()
     }
-    return(
-        <form onSubmit={handleSubmit} className="flex flex-col gap-3 lg:w-[75%] w-[95%] p-4 shadow-2xl hover:shadow-current transition-shadow">
-            <h1 className="lg:text-2xl md:text-xl text-md text-center font-extrabold text-gray-800 uppercase shadow-md shadow-red-400">Submit Below Details</h1>
-        <input type="text" placeholder="Enter Your Name" required className="h-6 px-3 py-5 outline-none focus:shadow-md focus:shadow-current
+    return (
+        <form
+            onSubmit={handleSubmit} className="flex flex-col gap-3 lg:w-[75%] w-[95%] p-4 shadow-2xl hover:shadow-current transition-shadow">
+            <motion.h1
+                initial={{ x: -1200 }}
+                animate={{ x: 0 }}
+                transition={{ delay: 0.5, duration: 1 }}
+                className="lg:text-2xl md:text-xl text-md text-center font-extrabold text-gray-800 uppercase shadow-md shadow-red-400">
+                Submit Below Details</motion.h1>
+            <input type="text" placeholder="Enter Your Name" required className="h-6 px-3 py-5 outline-none focus:shadow-md focus:shadow-current
        font-medium text-gray-500 text-xl transition-all"/>
-        <input type="email" placeholder="Enter Your Email" required className="h-6 px-3 py-5 outline-none focus:shadow-md focus:shadow-current
+            <input type="email" placeholder="Enter Your Email" required className="h-6 px-3 py-5 outline-none focus:shadow-md focus:shadow-current
        font-medium text-gray-500 text-xl transition-all"/>
-        <input type="number" placeholder="Enter Your Mobile" required className="h-6 px-3 py-5 outline-none focus:shadow-md focus:shadow-current
+            <input type="number" placeholder="Enter Your Mobile" required className="h-6 px-3 py-5 outline-none focus:shadow-md focus:shadow-current
        font-medium text-gray-500 text-xl transition-all"/>
-        <textarea name="" id="" cols="30" rows="6" required placeholder="Write Message..."
-        className="outline-none p-2 focus:shadow-md focus:shadow-current text-gray-500 text-xl font-medium"></textarea>
-        <button className="bg-teal-300 font-extrabold text-2xl
+            <textarea name="" id="" cols="30" rows="6" required placeholder="Write Message..."
+                className="outline-none p-2 focus:shadow-md focus:shadow-current text-gray-500 text-xl font-medium"></textarea>
+            <button className="bg-teal-300 font-extrabold text-2xl
      text-yellow-50 py-1 rounded-md hover:bg-teal-600 transition-all">Send Now</button>
         </form>
     )
--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -1,70 +1,92 @@
 "use client"
 import { animate, motion } from 'framer-motion'
+import Image from 'next/image'
 
 import { useState } from "react"
 
-export const Menu = () => {
-    const [open, setOpen] = useState(false)
-    const lists = [
-        {
-            id: 1,
-            name: "Home"
-        },
-        {
-            id: 2,
-            name: "Company +",
-            companySubLists:[
-                {
-                id:1,
-                name:"About Us",
+const lists = [
+    {
+        id: 1,
+        name: "Home"
+    },
+    {
+        id: 2,
+        name: "Company +",
+        companySubLists: [
+            {
+                id: 1,
+                name: "About Us",
             },
-                {
-                id:2,
-                name:"Our Gallery",
+            {
+                id: 2,
+                name: "Our Gallery",
             },
         ]
+    },
+    {
+        id: 3,
+        name: "Services +",
+        serviceSubLists: [
+            {
+                id: 1,
+                name: "Digital Marketing",
+            },
+            {
+                id: 2,
+                name: "Website Design",
+            },
+            {
+                id: 2,
+                name: "Content Writing",
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: "Packages +",
+        packageSubLists: [
+            {
+                id: 1,
+                name: "SEO Packages",
+            },
+        ]
+    },
+    {
+        id: 5,
+        name: "Career"
+    },
+    {
+        id: 6,
+        name: "Blog"
+    },
+    {
+        id: 7,
+        name: "Contact Us"
+    },
+]
+export const Menu = () => {
+    const [open, setOpen] = useState(false)
+    const [ulOpen, setUlOpen] = useState(false)
+    const listVarient = {
+        closed: {
+            x: "100vw",
         },
-        {
-            id: 3,
-            name: "Services +"
-        },
-        {
-            id: 4,
-            name: "Packages +"
-        },
-        {
-            id: 5,
-            name: "Career"
-        },
-        {
-            id: 6,
-            name: "Blog"
-        },
-        {
-            id: 7,
-            name: "Contact Us"
-        },
-    ]
-    const listVarient={
-        closed:{
-            x:"100vw",
-        },
-        opened:{
-            x:0,
-            transition:{
-                when:"beforeChildren",
-                StaggerChildren:0.2
+        opened: {
+            x: 0,
+            transition: {
+                when: "beforeChildren",
+                StaggerChildren: 0.2
             }
         },
     }
-    const listItemsVarients={
-        closed:{
-            x:-20,
-            opacity:0
+    const listItemsVarients = {
+        closed: {
+            x: -20,
+            opacity: 0
         },
-        opened:{
-            x:0,
-            opacity:1
+        opened: {
+            x: 0,
+            opacity: 1
         }
     }
     return (
@@ -82,15 +104,25 @@ export const Menu = () => {
                             animate={{ x: 0, y: 0 }}
                             transition={{ duration: 1.5, type: "spring", stiffness: 100 }}
                             className="relative mli list-none cursor-pointer px-2 py-1 text-md font-medium" key={list.id}>
-                                {list.name}
-                                <motion.ul
-                                className='absolute top-14 w-full flex flex-col gap-1 left-0 shadow-xl shadow-gray-300 rounded-md'>
-                        {
-                            list.companySubLists?.map((li)=>
-                            <li className=' text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
-                            )
-                        }
-                        </motion.ul>
+                            {list.name}
+                            <motion.ul
+                                className='absolute w-52 top-14 flex flex-col gap-1 left-0 shadow-xl shadow-gray-300 rounded-md'>
+                                {
+                                    list.companySubLists?.map((li) =>
+                                        <li className=' text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
+                                    )
+                                }
+                                {
+                                    list.serviceSubLists?.map((li) =>
+                                        <li className='whitespace-nowrap text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
+                                    )
+                                }
+                                {
+                                    list.packageSubLists?.map((li) =>
+                                        <li className=' text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
+                                    )
+                                }
+                            </motion.ul>
                         </motion.li>
                     )
 
@@ -101,18 +133,38 @@ export const Menu = () => {
 
             {
                 open &&
-                <motion.ul 
-                variants={listVarient}
-                initial="closed"
-                animate="opened"
-                className="bg-cyan-200 w-full lg:hidden h-screen py-5 absolute top-20 left-0 flex justify-start items-center flex-col gap-2">
+                <motion.ul
+                    variants={listVarient}
+                    initial="closed"
+                    animate="opened"
+                    className="bg-cyan-200 w-full lg:hidden h-screen py-5 absolute top-20 left-0 flex justify-start items-center flex-col gap-2">
                     {
                         lists.map((list) =>
                             <motion.li
-                            variants={listItemsVarients}
-                                className="bg-red-200 text-gray-700 shadow-md w-48 list-none cursor-pointer px-2 py-1 text-md font-medium rounded-md"
+                                variants={listItemsVarients}
+                                className={`mlist relative bg-red-200 text-gray-700 shadow-md w-48 list-none cursor-pointer px-2 py-1 text-md font-medium rounded-md`}
                                 key={list.id}>
-                                    {list.name}
+                                {list.name}
+                              
+                                <motion.ul
+                                className='absolute top-20 left-0 flex flex-col gap-1 shadow-xl shadow-gray-300 rounded-md'>
+                                {
+                                    list.companySubLists?.map((li) =>
+                                        <li className=' text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
+                                    )
+                                }
+                                {
+                                    list.serviceSubLists?.map((li) =>
+                                        <li className='whitespace-nowrap text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
+                                    )
+                                }
+                                {
+                                    list.packageSubLists?.map((li) =>
+                                        <li className=' text-left py-1 px-2 font-medium text-gray-700 hover:bg-slate-200' key={li.id}>{li.name}</li>
+                                    )
+                                }
+                            </motion.ul>
+
                             </motion.li>
                         )
 
